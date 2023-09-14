dc2bee026c79921e67fea7687b2be1789190747d
manansaipi <abdul.saipi@student.president.ac.id>
2023-09-14 16:15:36 +0700

    set up scheduler to take data from database to dataset

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// Import necessary libraries
+const mysql = require('mysql2');
+
+// Define database connection configurations for source and destination databases
+const sourceDbConfig  = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'database',
+});
+
+const destinationDbConfig  = mysql.createConnection( {
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'dataset',
+});
+
+// Create a function to transfer data from the source to the destination database
+
+// Function to transfer data
+function transferData() {
+  // Retrieve data from the source database
+  sourceDbConfig.query('SELECT * FROM r_u_u_s', (err, results) => {
+    if (err) throw err;
+
+    // Insert data into the destination database
+    results.forEach((row) => {
+      destinationDbConfig.query('INSERT INTO r_u_u_s SET ? ON DUPLICATE KEY UPDATE ?', [row, row], (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+}
+
+// Function to periodically run the data transfer
+function runDataTransfer() {
+  transferData();
+
+  // Schedule the next data transfer in 15 seconds
+  setTimeout(runDataTransfer, 15000); // 15 seconds
+  console.log('Data transfer success');
+
+}
+
+// Start the data transfer
+console.log('Data transfer started');
+runDataTransfer();
+
