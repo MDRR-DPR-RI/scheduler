3893db3dab40475bdefdd04d1ed8019c5f4ad1bb
manansaipi <abdul.saipi@student.president.ac.id>
2023-09-20 20:47:29 +0700

    add new branch. hit-api scheduler

--- src/index.js
+++ src/index.js
@@ -1,49 +1,47 @@
 // Import necessary libraries
 const mysql = require('mysql2');
+const axios = require('axios'); // Import Axios for making API requests
 
 // Define database connection configurations for source and destination databases
-const sourceDbConfig  = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'big_database',
-});
-
-const destinationDbConfig  = mysql.createConnection( {
+const destinationDbConfig = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'dataset',
 });
 
-// Create a function to transfer data from the source to the destination database
-
-// Function to transfer data
-function transferData() {
-  // Retrieve data from the source database
-  sourceDbConfig.query('SELECT * FROM ruus', (err, results) => {
-    if (err) throw err;
+// Create a function to fetch data from the API
+async function fetchDataFromAPI() {
+  try {
+    const response = await axios.get('http://localhost:3000/database'); // API URL
+    const data = response.data.data; // Extract the "data" array from the API response
 
     // Insert data into the destination database
-    results.forEach((row) => {
-      destinationDbConfig.query('INSERT INTO ruus SET ? ON DUPLICATE KEY UPDATE ?', [row, row], (err) => {
-        if (err) throw err;
+    data.forEach((item) => {
+      const { judul, pengusul } = item;
+      const query = 'INSERT INTO ruus (judul, pengusul) VALUES (?, ?)';
+
+      destinationDbConfig.query(query, [judul, pengusul], (err) => {
+        if (err) {
+          console.error('Error inserting data:', err);
+        }
       });
     });
-  });
+
+    console.log('Data transfer success');
+  } catch (error) {
+    console.error('Error fetching data from the API:', error);
+  }
 }
 
 // Function to periodically run the data transfer
 function runDataTransfer() {
-  transferData();
+  fetchDataFromAPI();
 
   // Schedule the next data transfer in 15 seconds
   setTimeout(runDataTransfer, 15000); // 15 seconds
-  console.log('Data transfer success');
-
 }
 
 // Start the data transfer
 console.log('Data transfer started');
 runDataTransfer();
-
